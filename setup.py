12af8853f704246fa5c909cccacc25b8c6a50098
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2026-07-06 09:58:57 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = [
 
 setuptools.setup(
     name="iiiflow",
-    version="1.1.1",
+    version="1.2.0",
     author="Gregory Wiedeman",
     author_email="gwiedeman@albany.edu",
     description="An IIIF pipeline tool using the Digital Object Discovery Storage Specification.",
